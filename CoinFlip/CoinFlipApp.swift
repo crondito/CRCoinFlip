b8ecae720922e387e6b51b46a4162893cdcd568d
Carlos Rodas <crods57@gmail.com>
2025-11-28 13:40:44 +0900

    Initial Commit

---
+++ CoinFlip/CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipApp.swift
+//  CoinFlip
+//
+//  Created by Carlos Rodas on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinFlip
+//
+//  Created by Carlos Rodas on 2025/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
