217a62275a1979b266f1eadac2128be572fe5310
Carlos Rodas <crods57@gmail.com>
2026-06-26 13:39:07 +0900

    removed EmptyScene and using EmptyView in CoinFlipApp

--- CoinFlip/CoinFlipApp.swift
+++ CoinFlip/CoinFlipApp.swift
@@ -13,17 +13,8 @@ struct CoinFlipApp: App {
     private let statusBarController = StatusBarController()
 
     var body: some Scene {
-        // Use EmptyScene to prevent default window creation; logic is in StatusBarController.
-        EmptyScene()
-    }
-}
-
-// Replacement Scene to prevent default window creation.
-struct EmptyScene: Scene {
-    var body: some Scene {
-        // Dummy window, never shown.
-        WindowGroup {
-            Color.clear.frame(width: 0, height: 0).hidden()
+        Settings {
+            EmptyView()
         }
     }
 }
